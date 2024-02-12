284e722cb0131ac199c79d0e721da7ed286096b1
bachidze <bachidze90@gmail.com>
2024-02-12 14:30:45 +0400

    Feat(Add):serverside-clientside

---
+++ app/clientone/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+const Clientone = () => {
+  
+  return (
+    <div>
+      <button onClick={() => alert('hello')}>Click Me</button>
+    </div>
+  )
+}
+
+export default Clientone
---
+++ app/servertwo/page.jsx
@@ -0,0 +1,10 @@
+
+
+const Servertwo = () => {
+    console.log('hello from (ServerComp)')
+  return (
+    <div>Servertwo</div>
+  )
+}
+
+export default Servertwo
